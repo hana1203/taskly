093033a755469a9b7369402e2ae40e4b94bc0862
hana1203 <hanaindec@gmail.com>
2023-03-13 20:21:05 +0900

    refactor: interface made for managing filtered list

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -8,3 +8,17 @@ export interface TaskListStateProps {
   tasklist: Task[];
   setTasklist: React.Dispatch<React.SetStateAction<Task[]>>;
 }
+
+export interface FilterListSetProps {
+  setIsFiltered?: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsInprogress?: React.Dispatch<React.SetStateAction<boolean>>;
+  setActivelist: React.Dispatch<React.SetStateAction<Task[]>>;
+  setCompletedlist: React.Dispatch<React.SetStateAction<Task[]>>;
+}
+
+export interface FilterListDataProps {
+  isFiltered: boolean;
+  isInprogress: boolean;
+  activelist: Task[];
+  completedlist: Task[];
+}
